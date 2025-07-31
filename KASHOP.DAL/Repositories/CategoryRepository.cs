2addf80d16368f6aaf2420ae5cc68c77dbf66671
ahedmafarjeh <ahedmafarjeh10@gmail.com>
2025-07-31 15:57:44 +0300

    Create Identity: Add users,roles

---
+++ KASHOP.DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using KASHOP.DAL.Data;
+using KASHOP.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.DAL.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly ApplicationDbContext context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public ApplicationDbContext Context { get; }
+
+        public Category GetById(int id)
+        {
+
+        }
+    }
+}
